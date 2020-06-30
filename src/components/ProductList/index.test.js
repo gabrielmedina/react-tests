982f63fd55a31dfc17cb994d32bb66943670c443
Gabriel Medina <gmedina.santos@gmail.com>
2020-06-30 19:24:22 -0400

    Add test case to ProductList

---
+++ src/components/ProductList/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductList from './'
+
+describe('Testes para produtos', () => {
+  it('Deve adicionar um novo produto na lista quando o formulário for submetido', () => {
+    // renderizar o componente
+    // buscar pelo input
+    // digitar o nome do novo produto no input
+    // buscar pelo botão de submissão
+    // clicar no botão
+    // buscar a lista de produtos
+    // verificar se o produto foi adicionado
+  })
+})
